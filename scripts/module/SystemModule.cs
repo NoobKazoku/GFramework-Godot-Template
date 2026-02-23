2249c669c7e00fe873f02e899a40b75ccdea6659
GeWuYou <95328647+GeWuYou@users.noreply.github.com>
2026-02-23 15:22:45 +0800

    refactor(modules): 将模块基类从AbstractModule更改为IArchitectureModule
    - 更新ModelModule、StateModule、SystemModule和UtilityModule继承IArchitectureModule接口
    - 移除所有模块中Install方法的override关键字
    - 删除无用的GFramework.Game.architecture命名空间引用
    - 更新GFramework相关包依赖版本从0.0.192升级到0.0.193
    - 更新Meziantou.Analyzer包版本从3.0.10升级到3.0.15

--- scripts/module/SystemModule.cs
+++ scripts/module/SystemModule.cs
@@ -1,5 +1,4 @@
 ﻿using GFramework.Core.Abstractions.architecture;
-using GFramework.Game.architecture;
 using GFramework.Game.setting;
 using GFrameworkGodotTemplate.scripts.core.audio.system;
 using GFrameworkGodotTemplate.scripts.core.scene;
@@ -11,13 +10,13 @@ namespace GFrameworkGodotTemplate.scripts.module;
 ///     系统Godot模块类，负责安装和注册游戏所需的各种系统组件
 ///     继承自AbstractGodotModule，用于在游戏架构中集成系统功能
 /// </summary>
-public class SystemModule : AbstractModule
+public class SystemModule : IArchitectureModule
 {
     /// <summary>
     ///     安装方法，用于向游戏架构注册各种系统组件
     /// </summary>
     /// <param name="architecture">游戏架构接口实例，用于注册系统</param>
-    public override void Install(IArchitecture architecture)
+    public void Install(IArchitecture architecture)
     {
         architecture.RegisterSystem(new UiRouter());
         architecture.RegisterSystem(new SceneRouter());
